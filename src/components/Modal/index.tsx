13a1895bc6283668a02778818da629453326b979
michalbilicki <michel.bilicki@gmail.com>
2022-12-07 19:26:19 +0100

    Create basic modal component

---
+++ src/components/Modal/index.tsx
@@ -0,0 +1,47 @@
+import { Button, Modal, Typography } from '@mui/material'
+import { ReactElement } from 'react'
+import { ModalBody, ModalContent, ModalFooter, ModalHeader } from './styles'
+
+interface ModalProps {
+	title?: string
+	open: boolean
+	handleConfirm?: () => void
+	handleClose: () => void
+	children: ReactElement
+}
+
+export const ModalComponent = ({
+	title,
+	open,
+	handleConfirm,
+	handleClose,
+	children,
+}: ModalProps) => {
+	return (
+		<Modal
+			open={open}
+			onClose={handleClose}
+			aria-labelledby="modal-modal-title"
+			aria-describedby="modal-modal-description"
+		>
+			<ModalContent>
+				{title && (
+					<ModalHeader>
+						<Typography variant="h5">{title}</Typography>
+					</ModalHeader>
+				)}
+				<ModalBody>{children}</ModalBody>
+				<ModalFooter>
+					{handleConfirm && (
+						<Button variant="contained" onClick={handleConfirm}>
+							Confirm
+						</Button>
+					)}
+					<Button variant="outlined" onClick={handleClose} color="error">
+						Close
+					</Button>
+				</ModalFooter>
+			</ModalContent>
+		</Modal>
+	)
+}
---
+++ src/components/Modal/styles.ts
@@ -0,0 +1,39 @@
+import styled from 'styled-components'
+
+export const ModalContent = styled.div`
+	position: absolute;
+	top: 50%;
+	left: 50%;
+	transform: translate(-50%, -50%);
+	box-shadow: 24;
+	border-radius: 0.25rem;
+	min-width: 40rem;
+	background-color: white;
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+`
+
+export const ModalHeader = styled.div`
+	position: relative;
+	padding: 1.25rem;
+	border-bottom: 1px solid gray;
+`
+
+export const ModalBody = styled.div`
+	padding: 1.25rem;
+	min-height: 15rem;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+`
+
+export const ModalFooter = styled.div`
+	position: relative;
+	padding: 1.25rem;
+	border-top: 1px solid gray;
+	display: flex;
+	justify-content: flex-end;
+	gap: 1rem;
+`
